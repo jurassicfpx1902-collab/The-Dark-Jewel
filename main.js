ef48f367b69e6cff2a04d2f8a9f70f7063f223e7
jurassicfpx1902-collab <jurassicfpx1902@gmail.com>
2026-09-21 23:37:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,9 @@
+"use strict";
+
 window.addEventListener(
     "DOMContentLoaded",
-    () => {
-
-        console.log(
-            "[THE DARK JEWEL] Inicializando..."
-        );
-
+    ()=>{
         AudioSystem.init();
-
-        UISystem.init();
-
         Game.init();
-
-        console.log(
-            "[THE DARK JEWEL] Todos os módulos carregados."
-        );
-
-        console.log(
-            "AudioSystem:",
-            !!window.AudioSystem
-        );
-
-        console.log(
-            "CollisionSystem:",
-            !!window.CollisionSystem
-        );
-
-        console.log(
-            "VisionSystem:",
-            !!window.VisionSystem
-        );
-
-        console.log(
-            "Player:",
-            !!window.Player
-        );
-
-        console.log(
-            "Enemy:",
-            !!window.Enemy
-        );
-
-        console.log(
-            "MissionSystem:",
-            !!window.MissionSystem
-        );
-
-        console.log(
-            "UISystem:",
-            !!window.UISystem
-        );
-
-        console.log(
-            "Game:",
-            !!window.Game
-        );
     }
 );
